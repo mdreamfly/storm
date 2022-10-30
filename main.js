40adffd390cf7995e710224464981622f267c813
mdreamfly <m.dreamfly@gmail.com>
2022-10-30 17:56:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ let body = JSON.parse($response.body);
 let result = JSON.parse(body['result']);
 result='';
 $done({body:JSON.stringify(body)})
+$notification.post("Storm Sniffer","Network Debug Tool","全新V2.0 即将上线!!!")
