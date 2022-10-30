d82bc44c145fa950d29d88d6f7e3fa1362ad9f5b
mdreamfly <m.dreamfly@gmail.com>
2022-10-30 18:07:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let body = JSON.parse($response.body);
 let result = JSON.parse(body['result']);
-result='';
+let healthRecord = JSON.parse(result['healthRecord']);
+healthRecord['colorCss']='#FF9F00'
+let result_new = JSON.stringify(result) 
 $done({body:JSON.stringify(body)})
-$notification.post("Storm Sniffer","Network Debug Tool","全新V2.0 即将上线!!!")
+$notification.post("Storm Sniffer","Network Debug Tool",result_new)
